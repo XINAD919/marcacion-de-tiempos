e9e4ea146717ec96be7a80ed3584d154b902b89e
Daniel Castaño <dcastano@matec.com.co>
2026-09-21 20:42:00 -0500

    Add Admin model and migration for staff login

---
+++ prisma/migrations/20260922014143_add_admin/migration.sql
@@ -0,0 +1,28 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Admin] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000) NOT NULL,
+    [nombre] NVARCHAR(1000) NOT NULL,
+    [passwordHash] NVARCHAR(1000) NOT NULL,
+    [activo] BIT NOT NULL CONSTRAINT [Admin_activo_df] DEFAULT 1,
+    [creadoEn] DATETIME2 NOT NULL CONSTRAINT [Admin_creadoEn_df] DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT [Admin_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Admin_email_key] UNIQUE NONCLUSTERED ([email])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
